62696e6562c15c8aaec568eb11b41ae5c62332ba
Kashif kazmi <kazmikashif007@gmail.com>
2025-07-21 10:14:33 +0530

    files

---
+++ template/header.php
@@ -0,0 +1,11 @@
+<div class="bg-primary position-relative">
+  <div class="overlay-gradient"></div>
+  <div class="container position-relative">
+  	<div class="row py-5">
+    	<div class="col">
+        <h1 class="display-1">QR Code Generator</h1>
+       
+    </div>
+    </div>
+  </div>
+</div>
